48a3e6b756ef132fdd99c3b6180ea739dd561b62
Sushil Chaudhari <sushil.chaudhari@progress.com>
2024-11-11 17:41:16 +0530

    second

--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "mymodules/mymodules/dog"
+
 func Bark() string {
 	return ("Bho!!")
 }
 
 func Barks() string {
 	return ("Bho Bho !")
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
